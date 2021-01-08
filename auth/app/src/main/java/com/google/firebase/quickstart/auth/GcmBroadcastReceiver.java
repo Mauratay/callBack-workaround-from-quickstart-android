41c85d22c75529641965d4c104be92e0ddb7406c
Mauratay <francisco.mauratay@gmail.com>
2021-01-08 07:08:02 -0600

    callback workaround

---
+++ auth/app/src/main/java/com/google/firebase/quickstart/auth/GcmBroadcastReceiver.java
@@ -0,0 +1,13 @@
+package com.google.firebase.quickstart.auth;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class GcmBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
+
